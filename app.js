aab2a06e989fbaa894e76caf3a65d68b075ea513
Braulio2026 <ortizcastrobrauliojose6@gmail.com>
2026-06-01 16:38:36 -0600

    feat(feature-branch): JS function load-events, floating button conected to user publications.(fast searching on the screen.)

--- app.js
+++ app.js
@@ -271,7 +271,7 @@ function safeUrl(url = "") {
     });
   }
 
-   // =======================
+// =======================
 // LOAD POSTS
 // =======================
 
@@ -332,6 +332,71 @@ async function loadPosts() {
     ); // <- IMPORTANT MISSING )
 
   renderPosts(postsWithLikes);
+}
+
+/*  load post to button events*/
+async function loadEvents() {
+
+  const container =
+    document.getElementById("events-list");
+
+  if (!container) return;
+
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .order("created_at", {
+      ascending: false
+    });
+
+  if (error) {
+    console.error(
+      "❌ Error loading events:",
+      error.message
+    );
+    return;
+  }
+
+  container.innerHTML = "";
+
+  data.forEach(event => {
+
+    const div = document.createElement("div");
+
+    div.classList.add("event-item");
+
+    div.innerHTML = `
+      <div class="event-card">
+
+        <button class="toggle-btn">
+          ${event.title}
+        </button>
+
+        <div class="event-info">
+
+          <p>
+            <strong>Date:</strong>
+            ${event.date}
+          </p>
+
+          <p>
+            <strong>Location:</strong>
+            ${event.location}
+          </p>
+
+          <p>
+            ${event.description}
+          </p>
+
+        </div>
+
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+
+  activateToggle();
 }
 
 // =======================
@@ -674,6 +739,7 @@ if (userPosts) {
           );
 
           await loadPosts();
+          await loadEvents();
 
         } catch (err) {
 
@@ -724,4 +790,5 @@ if (userPosts) {
   // =======================
 
   loadPosts();
+  loadEvents(); 
 });
